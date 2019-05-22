e25c95bc26120b22d74b3f84649c48f782996087
faleinshah <50477528+faleinshah@users.noreply.github.com>
2019-05-22 09:09:06 +0800

    Delete hello.rb

--- hello.rb
+++
@@ -1,8 +0,0 @@
-def reverse_string(string)
-  split_string = string.split("")
-  reversed = []
-  string.size.times { reversed << split_string.pop }
-  reversed.join
-end
-
-puts reverse_string("Hello")
